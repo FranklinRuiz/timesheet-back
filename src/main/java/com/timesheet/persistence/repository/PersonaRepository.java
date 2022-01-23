f3ac5a46455e4ec3b68b6cfc8f1dc9f6ef6b1a01
minsur <criizt.mart@gmail.com>
2022-01-22 23:05:40 -0500

    backend del modulo personas

--- src/main/java/com/timesheet/persistence/repository/PersonaRepository.java
+++ src/main/java/com/timesheet/persistence/repository/PersonaRepository.java
@@ -1,9 +1,12 @@
 package com.timesheet.persistence.repository;
 
 import com.timesheet.persistence.entity.Persona;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PersonaRepository extends JpaRepository<Persona,Long> {
+    Page<Persona> findByFlgActivo(boolean activo, Pageable pageable);
 }
