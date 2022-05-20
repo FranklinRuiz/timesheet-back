87ea3b82b90aad6701285a86d9be7ef26aea8044
minsur <criizt.mart@gmail.com>
2022-05-19 23:56:26 -0500

    modulos sdes

---
+++ src/main/java/com/sv/ts/service/TipoTrabajoService.java
@@ -0,0 +1,22 @@
+package com.sv.ts.service;
+
+import com.sv.ts.dto.TipoTrabajoDto;
+import com.sv.ts.persistence.model.TipoTrabajoModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface TipoTrabajoService {
+
+    TipoTrabajoModel saveTipoTrabajo(TipoTrabajoModel tipoTrabajoModel);
+
+    TipoTrabajoModel updateTipoTrabajo(TipoTrabajoModel tipoTrabajoModel);
+
+    Page<TipoTrabajoDto> listAllByPage(Pageable pageable, String nombre);
+
+    boolean deleteTipoTrabajo(Long idTipoTrabajo);
+
+    List<TipoTrabajoDto> listTipoTrabajo();
+
+}
